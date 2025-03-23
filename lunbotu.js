f73c57f95cdf94856fdd03e0d79d49fd19b09086
Crystal666988 <xiezihaoxieni@163.com>
2025-03-23 10:45:04 +0800

    Add files via upload

---
+++ lunbotu.js
@@ -0,0 +1,49 @@
+const prev=document.querySelector('.prev')
+const next=document.querySelector('.next')
+const image=document.querySelector('.images')
+const images=[
+ '1.jpg','2.jpg','3.gif','4.jpg','5.jpg','6.jpg','7.jpg','a.jpg','b.jpg'
+]/*创建图片数组，其中这是轮播图部分的，照片可自己添加*/
+for(let i=0;i<images.length;i++){
+ let img=document.createElement('img')/*创建img元素，然后添加这些元素到lunbotu类的子代*/
+ img.src=images[i]
+ img.className='item'/*取名为item类*/
+ image.appendChild(img)
+}
+function movenext(){
+ let items=document.querySelectorAll('.item')/*选取所有的item类*/
+ image.append(items[0])/*轮播图逻辑，只要鼠标向上轮动或者按了前进按钮，就自动把第一个数组元素放到数组的末尾，以此实现轮播*/
+}
+function moveprev(){
+ let items=document.querySelectorAll('.item')
+ image.prepend(items[items.length-1])/*同理，把末尾的元素放到第一位*/
+}
+window.addEventListener('wheel',function(event){
+     if(event.deltaY>0){
+         movenext()
+     }else{
+         moveprev()
+     }
+ })/*wheel是检测鼠标滚轮，向上就触发movenext函数*/
+ // 添加自动轮播功能
+ let autoPlayInterval = null;/*控制轮播的开关变量*/
+ function startAutoPlay() {
+     // 每3秒自动轮播一次
+     autoPlayInterval = setInterval(movenext, 3000);
+ }
+ function stopAutoPlay() {
+     if (autoPlayInterval) {
+         clearInterval(autoPlayInterval);/*停止轮播图函数*/
+         autoPlayInterval = null;
+     }
+ }
+
+ // 鼠标移入盒子时暂停自动轮播，移出时继续自动轮播
+ image.addEventListener('mouseenter', stopAutoPlay);
+ image.addEventListener('mouseleave', startAutoPlay);
+
+ // 开始自动轮播
+ startAutoPlay();
+
+ prev.addEventListener('click',moveprev)
+ next.addEventListener('click',movenext)
